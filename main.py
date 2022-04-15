e21bdf4b85381c6b85f543ce18319c75cc05b1f4
JoeTinnySpace <josephdennisjoo71@gmail.com>
2022-04-15 06:00:02 +0530

    comments

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
 print("This is not a script")
 
 def dataframe_loader(possible_sheetname):
-    # For selecting a particular Excel sheet from excel workbook (pandas, os, openpyxl)
-    # possible_sheetname = ["raw data", "fake tids"]
+    ## For selecting a particular Excel sheet from excel workbook (pandas, os, openpyxl)
+    ## possible_sheetname = ["raw data", "fake tids"]
 
     from pandas import read_excel
     from openpyxl import load_workbook
     import os
 
     files = os.listdir(os.getcwd())
-    # list of all files in working directory
-
     files_xlsx = [file for file in files if f[-4:] == 'xlsx']
-    # looking for specific extension of files eg: .xlsx
-
     #loop through all the file(s) to check sheetname
     for f in files_xlsx:
         print('Checking sheetnames...')
@@ -27,6 +23,7 @@ def dataframe_loader(possible_sheetname):
             print("The file not found!")
 
 def send_telegram_msg(message, chatid, bot_token):
+    ## send message to any telegram user with chatid
     from requests import get
     if type(chatid) == list:
         for i in chatid:
@@ -39,6 +36,7 @@ def send_telegram_msg(message, chatid, bot_token):
         print(send_text)
 
 def ordered_json(json_file_loation):
+    ## Ordering a json file in alphabetical order
     from json import loads, dump
 
     with open(json_file_loation,'r+') as data:
